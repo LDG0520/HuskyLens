ddf723ad9ae745a2435e4aae15a9a9c205b40770
LDG0520 <113489359+LDG0520@users.noreply.github.com>
2022-09-21 11:53:43 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,60 @@
+function Face_Recognition () {
+    huskylens.initI2c()
+    huskylens.initMode(protocolAlgorithm.ALGORITHM_FACE_RECOGNITION)
+    while (초기화 == 0) {
+        huskylens.request()
+        if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+            basic.showIcon(IconNames.Heart)
+            basic.pause(100)
+            basic.clearScreen()
+            초기화 = 1
+        }
+    }
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.UntilDone)
+    basic.showString("HI")
+    basic.pause(100)
+    기분 = 0
+}
+input.onButtonPressed(Button.A, function () {
+    Tag_Recognition()
+})
+input.onButtonPressed(Button.AB, function () {
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.yawn), SoundExpressionPlayMode.UntilDone)
+    basic.showIcon(IconNames.Asleep)
+    basic.clearScreen()
+    기분 = 0
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(기분)
+    basic.clearScreen()
+})
+function Tag_Recognition () {
+    huskylens.initI2c()
+    huskylens.initMode(protocolAlgorithm.ALGORITHM_TAG_RECOGNITION)
+    while (기분 < 100) {
+        if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+            basic.showIcon(IconNames.Happy)
+            music.playSoundEffect(music.builtinSoundEffect(soundExpression.happy), SoundExpressionPlayMode.UntilDone)
+            basic.pause(2000)
+            basic.clearScreen()
+            기분 += 1
+        }
+        if (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+            basic.showIcon(IconNames.Surprised)
+            music.playSoundEffect(music.builtinSoundEffect(soundExpression.mysterious), SoundExpressionPlayMode.UntilDone)
+            basic.pause(2000)
+            basic.clearScreen()
+            기분 += 1
+        }
+        if (huskylens.isAppear(3, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+            basic.showIcon(IconNames.Sad)
+            music.playSoundEffect(music.builtinSoundEffect(soundExpression.sad), SoundExpressionPlayMode.UntilDone)
+            basic.pause(2000)
+            basic.clearScreen()
+            기분 += -1
+        }
+    }
+}
+let 기분 = 0
+let 초기화 = 0
+Face_Recognition()
